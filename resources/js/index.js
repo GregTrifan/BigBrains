4d107160262149d9527867248789413482d0edd9
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-03-16 20:04:00 +0200

    Added React support ☺️

---
+++ resources/js/index.js
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import App from "./App";
+import theme from "./theme";
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <StrictMode>
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <App />
+        </ChakraProvider>
+    </StrictMode>,
+    rootElement
+);

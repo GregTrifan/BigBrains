65f7d78b1b63779cf2a41b4aba7c23bf2267a557
nikolas <nikolasdan16@protonmail.com>
2021-03-23 18:52:33 +0200

    Am scos animatia ai nenorocita, stonks

--- resources/js/routes/Home.jsx
+++ resources/js/routes/Home.jsx
@@ -1,21 +1,9 @@
 import { Box, Text } from "@chakra-ui/layout";
-import { motion } from "framer-motion";
 const Home = () => {
     return (
-        <motion.div
-            initial={{ scaleY: 0 }}
-            animate={{ scaleY: 1, rotateY: 360 }}
-            transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "reverse",
-                type: "spring",
-            }}
-        >
-            <Box padding="1" fontSize="large" textAlign="center">
-                <Text fontSize="4xl">Welcome Home!</Text>
-            </Box>
-        </motion.div>
+        <Box padding="1" fontSize="large" textAlign="center">
+            <Text fontSize="4xl">Welcome Home!</Text>
+        </Box>
     );
 };
 export default Home;

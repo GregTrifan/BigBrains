9ad812fb6fc5268c54ffda8a16547a16e631692b
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-03-23 12:48:03 +0200

    Documented controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,35 +1,55 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Validator;
 use App\Models\Posts;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function index(Request $request) {
-        $posts = Posts::select('title','content','votes')->paginate(10);
+    /**
+     * Get 10 posts
+     * page is declared in `route/to/method/?page=id`
+     *@param Request $request
+     *@return Posts[10]
+     */
+    public function index(Request $request)
+    {
+        $posts = Posts::select('title', 'content', 'votes')->paginate(10);
         return response()->json($posts);
     }
-    public function create(Request $request) {
+    /**
+     * Create a post
+     *  @param Request $request
+     *  @return Posts
+     */
+    public function create(Request $request)
+    {
         $validator = Validator::make($request->all(), [
-            'title'=> 'required',
-            'content'=> 'required'
+            'title' => 'required',
+            'content' => 'required'
         ]);
-        if($validator->fails()) {
-            return response()->json(['err'=>$validator->errors()],400);
+        if ($validator->fails()) {
+            return response()->json(['err' => $validator->errors()], 400);
         }
         $input = $request->all();
-        $post=Posts::create($input);
-        return response()->json(["status"=>"Post uploaded sucessfully","id"=>$post->id]);
+        $post = Posts::create($input);
+        return response()->json(["status" => "Post uploaded sucessfully", "id" => $post->id]);
     }
-    public function show($id) {
-        $query=Posts::select("title","content","votes","views")->where("id","=",$id)->first();
+    /**
+     * Show a specific Post
+     * @param int $id
+     * @return Posts
+     */
+    public function show($id)
+    {
+        $query = Posts::select("title", "content", "votes", "views")->where("id", "=", $id)->first();
         if ($query) {
-            $query->views+=1;
+            $query->views += 1;
             $query->save();
             return response()->json($query);
         }
-        return response('',404);
+        return response('', 404);
     }
 }
